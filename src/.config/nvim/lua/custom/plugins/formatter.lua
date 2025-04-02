6fe008d1ceb9f1dee585856025c5556a7c5f1033
Yaroslav Plishan <288993@gmail.com>
2025-04-03 02:52:16 +0300

    yastyle finally works

--- src/.config/nvim/lua/custom/plugins/formatter.lua
+++ src/.config/nvim/lua/custom/plugins/formatter.lua
@@ -5,44 +5,17 @@ return {
       local util = require "formatter.util"
 
       local function yastyle()
+        local path = util.escape_path(util.get_current_buffer_file_path());
         return {
-          exe = "ya",
-          args = {
-            "style",
-            "--all",
-            "-q",
-            "--stdin-filename",
-            util.escape_path(util.get_current_buffer_file_name()),
-          },
+          exe = 'tr "" "" >/dev/null; zsh -c \'output="$(~/arcadia/ya style --yamake ' .. path .. ' --dry-run --no-diff 2>/dev/null | tail -n +3)"; [ -z "$output" ] && cat ' .. path .. ' || print "$output"\'',
           stdin = true,
+          no_append = true,
         }
-      end
-
-      if ARCADIA_MODE then
-        vim.cmd [[
-        augroup FormatAutogroup
-          autocmd!
-          autocmd User FormatterPost lua if vim.bo.filetype == "yamake" then vim.cmd("e") end
-        augroup END
-        ]]
       end
 
       require("formatter").setup {
         filetype = {
-          yamake = ARCADIA_MODE and {
-            function()
-              vim.api.nvim_err_writeln("Current filetype: " .. util.escape_path(util.get_current_buffer_file_name()))
-              return {
-                exe = "ya",
-                args = {
-                  "style",
-                  "--yamake",
-                  "-q",
-                  util.escape_path(util.get_current_buffer_file_name()),
-                },
-              }
-            end,
-          } or nil,
+          yamake = ARCADIA_MODE and yastyle or nil,
           go = {
             ARCADIA_MODE and yastyle or require("formatter.filetypes.go").gofmt,
           },
