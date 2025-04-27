e8a8fd0aa884cfb25ad8a5b69225c9ca388b9f17
Yaraslau Plishan <yplishan@MacBook-Pro.local>
2025-04-27 23:37:25 +0300

    transition to vscode, disable most of starship

--- src/.config/nvim/lua/custom/vscode/keymaps.lua
+++ src/.config/nvim/lua/custom/vscode/keymaps.lua
@@ -15,8 +15,8 @@ keymap("n", "gcc", function() vscode.call "editor.action.commentLine" end, opts)
 keymap("v", "gc", function() vscode.call "editor.action.commentLine" end, opts)
 
 -- Indent
-keymap("v", "<Tab>", function() vscode.call "editor.action.indentLines" end, opts)
-keymap("v", "<S-Tab>", function() vscode.call "editor.action.outdentLines" end, opts)
+keymap("v", ">", function() vscode.call "editor.action.indentLines" end, opts)
+keymap("v", "<", function() vscode.call "editor.action.outdentLines" end, opts)
 
 -- Lines
 keymap("v", "<leader>lf", function() vscode.call "editor.action.formatSelection" end, opts)
@@ -25,11 +25,21 @@ keymap("v", "<leader>ls", function() vscode.call "editor.action.sortLinesAscendi
 -- Code
 keymap("n", "<leader>cf", function() vscode.call "editor.action.formatDocument" end, opts)
 keymap("n", "<leader>ca", function() vscode.call "editor.action.quickFix" end, opts)
+keymap("n", "<leader>cr", function() vscode.call "editor.action.rename" end, opts)
 
 -- Find
-keymap("n", "<leader>fg", function() vscode.call "workbench.action.findInFiles" end, opts)
-keymap("n", "<leader>ff", function() vscode.call "workbench.action.quickOpen" end, opts)
-keymap("n", "<leader>fo", function() vscode.call "workbench.action.files.openFile" end, opts)
+keymap({ "n", "v" }, "<leader>bf", function() vscode.call "opened-editors.openedEditors" end, opts)
+keymap({ "n", "v" }, "<leader>ff", function() vscode.call "find-it-faster.findFiles" end, opts)
+keymap({ "n", "v" }, "<leader>fg", function() vscode.call "find-it-faster.findWithinFiles" end, opts)
+keymap({ "n", "v" }, "<leader>fG", function() vscode.call "find-it-faster.findWithinFilesWithType" end, opts)
+keymap({ "n", "v" }, "<leader>fr", function() vscode.call "find-it-faster.resumeSearch" end, opts)
+keymap({ "n", "v" }, "<leader>cs", function() vscode.call "editor.action.gotoSymbol" end, opts)
+
+-- File Browser
+keymap("n", "<leader>fe", function() vscode.call "vsnetrw.open" end, opts)
+
+-- Clangd
+keymap("n", "<leader>ch", function() vscode.call "clangd.switchheadersource" end, opts)
 
 -- Open
 keymap("n", "<leader>of", function() vscode.call "newFolder.new" end, opts)
@@ -37,6 +47,8 @@ keymap("n", "<leader>on", function() vscode.call "workbench.action.files.openFol
 
 -- Utils
 keymap("n", "<leader>h", ":nohlsearch<CR>", opts)
+keymap("n", "<leader>es", ":set spell!<CR>", opts)
+keymap("n", "<leader>ew", ":set wrap!<CR>", opts)
 
 -- Command mode unmap
 keymap("n", "q:", "", opts)
